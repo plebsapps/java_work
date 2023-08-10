3d630b25e656f1184b16fa21ccbf9a889620fd7b
plebsapps <balz@balz-software.de>
2023-08-10 12:35:27 +0200

    Initial commit

---
+++ src/main/java/JavaMenu/JavaMenu.java
@@ -0,0 +1,12 @@
+package JavaMenu;
+
+public class JavaMenu {
+
+    public static void main(String[] args){
+
+        String eingebendeTextzeile;
+        eingebendeTextzeile = Terminal.readString();
+    }
+
+
+}
---
+++ src/main/java/Kassenbon/Kassenbon.java
@@ -0,0 +1,47 @@
+package Kassenbon;
+
+public class Kassenbon {
+	public static void main(String[] args){
+		double wurst = 2.31;
+		double kaese = 3.54;
+		double brot  = 1.50;
+		double dvd = 12.00;
+		
+		int anz_wurst = 1;
+		int anz_kaese = 1;
+		int anz_brot  = 1;
+		int anz_dvd   = 2;
+		
+		double brieftasche = 50.00;
+		
+		double summe = 0;
+		
+		summe = summe + wurst * anz_wurst;
+		summe = summe + kaese * anz_kaese;
+		summe = summe + brot * anz_brot;
+		summe = summe + dvd * anz_dvd;
+		
+		if (brieftasche < summe){
+			System.out.println("Sie haben nicht genug Geld in Ihrer Brieftasche");
+		} else {
+			System.out.println(String.format("%-9s %2d x %5.2f CHF", "WURST", anz_wurst, wurst)); 
+			System.out.println(String.format("%30.2f CHF", anz_wurst * wurst));
+
+			System.out.println(String.format("%-9s %2d x %5.2f CHF", "Kaese", anz_kaese, kaese)); 
+			System.out.println(String.format("%30.2f CHF", anz_kaese * kaese));
+			
+			System.out.println(String.format("%-9s %2d x %5.2f CHF", "Brot", anz_brot, brot)); 
+			System.out.println(String.format("%30.2f CHF", anz_brot * brot));
+			
+			System.out.println(String.format("%-9s %2d x %5.2f CHF", "DVD", anz_dvd, dvd)); 
+			System.out.println(String.format("%30.2f CHF", anz_dvd * dvd));
+
+			System.out.println("---------------------------------------------------------------------");
+			System.out.println(String.format("%9s %20.2f CHF","SUMME", summe ));
+			System.out.println(String.format("%9s %20.2f CHF","Gegeben", brieftasche));
+			
+			System.out.println("");
+			System.out.println(String.format("%9s %20.2f CHF","Rueckgeld", brieftasche-summe));
+		}
+	}
+}
