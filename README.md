23b78b4704be3e7ca830b25d8e3db104c03e671c
plebsapps <balz@balz-software.de>
2023-08-10 13:27:57 +0200

    Edit JavaMenu

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Java Work ist ein sammlung von Java Aufgaben gestartet von EASY EASY zu ...
 ## Kassenbon
 
 [Kassenbon](https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Kassenbon) Javakurs/Übungsaufgaben/Kassenbon
+Die Lösung ist zu finden unter [Kassenbon](src/main/java/ch/plebsapps/Kassenbon/Kassenbon.java).
+
 
 ## JavaMenu
 
 [JavaMenu](https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Javamenue)
-Weitere Details finden Sie in der [JavaMenu Klasse](src/main/java/ch/plebsapps/JavaMenu/JavaMenu.java).
-
+Die Lösung ist zu finden unter [JavaMenu](src/main/java/ch/plebsapps/JavaMenu/JavaMenu.java).
+Die dazugehörige Klasse Terminal unter [Terminal](src/main/java/ch/plebsapps/JavaMenu/Terminal.java).
--- src/main/java/ch/plebsapps/JavaMenu/JavaMenu.java
+++ src/main/java/ch/plebsapps/JavaMenu/JavaMenu.java
@@ -2,11 +2,42 @@ package ch.plebsapps.JavaMenu;
 
 public class JavaMenu {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        //Text eingeben
         String eingebendeTextzeile;
+        System.out.println();
+        System.out.println("Wie ist dein Name? ");
+        // Einlesen einer Tastatureingabe bis zum nächsten Zeilenumbruch in die Variable "eingegebeneTextzeile"
         eingebendeTextzeile = Terminal.readString();
+        System.out.printf("Hallo %s%n", eingebendeTextzeile);
+
+        //Menu mit Zahlen
+        System.out.println();
+        System.out.println("Wählen Sie ihr Geschlecht");
+        System.out.println("1) eine Frau ist.");
+        System.out.println("2) ein Mann ist.");
+        System.out.println("3) eine andere Geschlechterzugehörigkeit hat. (z.B. \"anderes\")");
+        int eingegebeneZahl;
+        // Einlesen einer Zahl per Tastatureingabe in die Variable "eingegebeneZahl"
+        eingegebeneZahl = Terminal.readInt();
+
+        if (eingegebeneZahl == 1) {
+            System.out.printf("Hallo Frau %s%n", eingebendeTextzeile);
+        } else if (eingegebeneZahl == 2) {
+            System.out.printf("Hallo Herr %s%n", eingebendeTextzeile);
+        } else {
+            System.out.printf("Hallo du neutron %s%n", eingebendeTextzeile);
+        }
+
+        switch (eingegebeneZahl) {
+            case 1:
+                System.out.printf("Hallo Frau %s%n", eingebendeTextzeile);
+                break;
+            case 2:
+                System.out.printf("Hallo Herr %s%n", eingebendeTextzeile);
+                break;
+            default:
+                System.out.printf("Hallo du neutron %s%n", eingebendeTextzeile);
+        }
     }
-
-
 }
