5827147938a0b1751695d3c91b50917645fdced5
plebsapps <balz@balz-software.de>
2023-08-16 11:07:26 +0200

    Rangfolge von Operatoren

--- src/main/java/ch/plebsapps/operatoren/AuswertereihenfolgeOp.java
+++ src/main/java/ch/plebsapps/operatoren/AuswertereihenfolgeOp.java
@@ -1,29 +1,46 @@
 package ch.plebsapps.operatoren;
 
+/**
+ * Die Rangfolge von Operatoren kann für entscheidender Bedeutung
+ * sein welches Ergebnis kommt daher diese Liste der Rangfolge der
+ * Operatoren wie diese ausgewertet wird.
+ * -
+ * Zu empfehlen ist hier eigentlich immer es so zu Programmieren,
+ * dass dieses auch ohne die Liste ersichtlich ist,
+ * zum Beispiel mit Klammern oder entsprechend in der Reihenfolge im Code.
+ * -
+ * Je weiter oben ein Operator in der Tabelle auftaucht, desto eher wird er ausgewertet.
+ * Operatoren mit dem gleichen Rang (in der gleichen Zeile) werden von links nach rechts ausgewertet.
+ */
+@SuppressWarnings("all")
 public class AuswertereihenfolgeOp {
+
 
     public static void main(String[] args) {
 
-        //TODO
-
-        /*
-1	=, +=, -=, *= ...	Zuweisungsoperator
-2	?:	Bedingungsoperator
-3	||	Logische Oder
-4	&&	Logisches Und
-5	|	logisches oder bitweises Oder
-6	^	logisches oder bitweises Entweder-Oder
-7	&	logisches oder bitweises Und
-8	==, !=	Vergleichsoperatoren: Gleich, Ungleich
-9	<, <=, >, >=	Vergleichsoperatoren
-10	<<, >>, >>>	Schiebeoperatoren
-11	+, -	Addition, Subtraktion, Verketten von Zeichenketten
-12	*, /, %	Multiplikation, Division, Rest
-13	++, --, +, -, ~, !	unäre (einstellige) Operatoren
-
-         */
+        System.out.println("1\tPostfix-Operatoren, Postinkrement, Postdekrement\tx++, x--");
+        System.out.println("2\tEinstellige (unäre) Operatoren, Vorzeichen\t++x, --x, +x, -x, ~b, !b");
+        System.out.println("3\tMultiplikation, Teilerrest\ta*b, a/b, a % b");
+        System.out.println("4\tAddition, Subtraktion\ta + b, a - b");
+        System.out.println("5\tBitverschiebung\td << k, d >> k, d >>> k");
+        System.out.println("6\tVergleiche\ta < b, a > b, a <= b, a >= b, s instanceof S");
+        System.out.println("7\tGleich, Ungleich\ta == b, a != b");
+        System.out.println("8\tUND (Bits)\tb & c");
+        System.out.println("9\tExor (Bits)\tb ^ c");
+        System.out.println("10\tODER (Bits)\tb | c");
+        System.out.println("11\tLogisch UND\tB && C");
+        System.out.println("12\tLogisch ODER\tB || C");
+        System.out.println("13\tBedingungsoperator\ta ? b : c");
+        System.out.println("14\tZuweisungen\ta = b, a += 3, a -= 3, a *= 3, a /= 3, a %= 3, b &= c, b ^= c, b |= c, d <<=k, d >>= k, d >>>= k");
 
 
+        boolean a = true, b = false, ergebnis;
 
+        System.out.println("Beispiel:");
+        ergebnis = a & b | b;
+        System.out.println("a & b | b  - ergebnis: " + ergebnis);
+
+        ergebnis = a | b & b;
+        System.out.println("a | b & b  - ergebnis: " + ergebnis);
     }
 }
