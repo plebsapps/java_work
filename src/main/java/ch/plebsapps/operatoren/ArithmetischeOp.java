15081582dc95c7701cb817d377187d567324f50b
plebsapps <balz@balz-software.de>
2023-08-16 10:11:17 +0200

    Arithmetische Operatoren

--- src/main/java/ch/plebsapps/operatoren/ArithmetischeOp.java
+++ src/main/java/ch/plebsapps/operatoren/ArithmetischeOp.java
@@ -1,19 +1,45 @@
 package ch.plebsapps.operatoren;
 
+/**
+ * Arithmetische Operatoren
+ * Eigentlich relativ selbsterklärend, aber der vollständigkeitshalber.
+ * Folgend ein Beispiel:
+ */
 public class ArithmetischeOp {
 
-
     public static void main(String[] args) {
-        //TODO
-            /*
-    +	a + b	Addition: Summe von a und b
--	a - b	Subtraktion: Differenz an a und b
-*	a * b	Multiplikation: Produkt von a und b
-/	a /  b	Division: Quotient von a und b
-%	a % b	Modulo: Rest einer ganzzahligen Division von a durch b
-     */
+
+        int a = 10;
+        int b = 2;
+        int ergebnis;
+
+        // + a + b	Addition: Summe von a und b
+        ergebnis = a + b;
+        System.out.println("+ Ergebnis: " + ergebnis);
+
+        // -	a - b	Subtraktion: Differenz an a und b
+        ergebnis = a - b;
+        System.out.println("- Ergebnis: " + ergebnis);
+
+        // *	a * b	Multiplikation: Produkt von a und b
+        ergebnis = a * b;
+        System.out.println("* Ergebnis: " + ergebnis);
+
+        // /	a /  b	Division: Quotient von a und b
+        ergebnis = a / b;
+        System.out.println("/ Ergebnis: " + ergebnis);
+
+        // %	a % b	Modulo: Rest einer ganzzahligen Division von a durch b
+        a = 11;
+        ergebnis = a % b;
+        System.out.println("% Ergebnis: " + ergebnis);
+
+        // +	positives Vorzeichen, in der Regel überflüssig	int j = +3;
+        ergebnis = +a;
+        System.out.println("+ Vorzeichen Ergebnis: " + ergebnis);
+
+        // -	negatives Vorzeichen	int minusJ = -j;
+        ergebnis = -a;
+        System.out.println("- Vorzeichen Ergebnis: " + ergebnis);
     }
-
-
-
 }
