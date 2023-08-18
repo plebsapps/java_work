1e10791273ffcc324438e63c63f1c908c5dbcf7a
plebsapps <balz@balz-software.de>
2023-08-18 13:05:29 +0200

    Bit Operatoren

--- src/main/java/ch/plebsapps/operatoren/BitOp.java
+++ src/main/java/ch/plebsapps/operatoren/BitOp.java
@@ -5,40 +5,94 @@ public class BitOp {
 
     public static void main(String[] args) {
 
-        int i = 7;
-        String strI = Integer.toBinaryString(i);
-        System.out.printf(String.format("%032d",Integer.valueOf(strI))+ "\n");
-        System.out.println(Integer.toBinaryString(~i));
-
-        //TODO
-/*
-        int a = true;
-
-        System.out.println(a);
-        a=~a;
-        System.out.println(~a);
-*/
+        int a = 0b101010;
+        int b = 0b110101;
 
 
-        /*
-~	~a	Komplement
-&	a & b	Und
-|	a | b	Oder
-^	a ^b	exklusives Oder
-        */
+        //Anzeigen Binär von Logisch Komplementär
+        System.out.println("\n\n~a Komplementär ");
+        System.out.println("    a = " + format(a));
+        System.out.println("   ~a = "+ format(~a));
+
+        //Anzeigen Binär von Logisch UND &
+        System.out.println("\na  UND  b");
+        System.out.println("    a = " + format(a) );
+        System.out.println("    b = " + format(b) );
+        System.out.println("a & b = " + format(a & b) );
+
+        //Anzeigen Binär von Logisch ODER |
+        System.out.println("\na  ODER  b");
+        System.out.println("    a = " + format(a) );
+        System.out.println("    b = " + format(b) );
+        System.out.println("a | b = " + format(a | b) );
+
+        //Anzeigen Binär von Logisch XOR |
+        System.out.println("\na  XOR  b");
+        System.out.println("    a = " + format(a) );
+        System.out.println("    b = " + format(b) );
+        System.out.println("a ^ b = " + format(a ^ b) );
 
 
-        /*
-a	7	0 0000000 00000000 00000000 00000111
-b	6	0 0000000 00000000 00000000 00000110
-result = a & b	6	0 0000000 00000000 00000000 00000110
-         */
+        a  = -8;
+        b = 1;
 
-        /*
-a	7	0 0000000 00000000 00000000 00000111
-b	6	0 0000000 00000000 00000000 00000110
-result = a | b	7	0 0000000 00000000 00000000 00000111
-         */
+        //Linksverschiebung mit Vorzeichen  <<
+        System.out.println("\na  <<  b");
+        System.out.println("     a = " + format(a) + " " + a  );
+        System.out.println("     b = " + b );
+        System.out.println("a << b = " + format(a << b) + " " + (a << b)  );
+
+        b = 29;
+
+        //Linksverschiebung mit Vorzeichen  <<
+        System.out.println("\na  <<  b");
+        System.out.println("     a = " + format(a) + " " + a  );
+        System.out.println("     b = " + b );
+        System.out.println("a << b = " + format(a << b) + " " + (a << b) + " <- ACHTUNG es wird aus dem Binär herausgeschoben" );
+
+        a = -8;
+        b = 2;
+
+        //Rechtsverschiebung mit Vorzeichen  >>
+        System.out.println("\na  >>  b  links shift");
+        System.out.println("     a = " + format(a) + " " + a );
+        System.out.println("     b = " + b );
+        System.out.println("a >> b = " + format(a >> b) + " " + (a >> b));
+
+        a = 8;
+        b = 2;
+
+        //Rechtsverschiebung mit Vorzeichen  >>
+        System.out.println("\na  >>  b  links shift");
+        System.out.println("     a = " + format(a) + " " + a );
+        System.out.println("     b = " + b );
+        System.out.println("a >> b = " + format(a >> b) + " " + (a >> b));
+
+        b = 10;
+
+        //Rechtsverschiebung mit Vorzeichen  >>
+        System.out.println("\na  >>  b  rechts shift");
+        System.out.println("     a = " + format(a) + " " + a);
+        System.out.println("     b = " + b );
+        System.out.println("a >> b = " + format(a >> b) + " " + (a >> b) + " <- ACHTUNG es wird aus dem Binär herausgeschoben" );
+
+        a = -1;
+        b = 1;
+
+        //Logische Rechtsverschiebung  >>>
+        System.out.println("\na  >>>  b  Logische Rechtsverschiebung ");
+        System.out.println("     a = " + format(a) + " " + a );
+        System.out.println("     b = " + b );
+        System.out.println("a >>> b = " + format(a >>> b) + " " + (a >>> b) + " <- ACHTUNG es ändert sich das Vorzeichen LOGISCH");
+
+        a = -8;
+        b = 2;
+
+        //Logische Rechtsverschiebung  >>>
+        System.out.println("\na  >>>  b  Logische Rechtsverschiebung ");
+        System.out.println("     a = " + format(a) + " " + a );
+        System.out.println("     b = " + b );
+        System.out.println("a >>> b = " + format(a >>> b) + " " + (a >>> b) + " <- ACHTUNG es wird aus dem Binär herausgeschoben" );
 
 
         /*
@@ -49,4 +103,10 @@ result = a | b	7	0 0000000 00000000 00000000 00000111
 
 
     }
+
+    //Zeigt Integer als Binäre zahl mit führenden Nullen
+    public static String format(int a){
+        return String.format("%32s", Integer.toBinaryString(a)).replace(' ', '0' );
+    }
+
 }
