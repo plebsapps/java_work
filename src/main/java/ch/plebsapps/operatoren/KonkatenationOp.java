f7f6e4f480c95c7ebcbd29e7d81ef8debc929e16
plebsapps <balz@balz-software.de>
2023-08-18 10:11:33 +0200

    Kommentare

--- src/main/java/ch/plebsapps/operatoren/KonkatenationOp.java
+++ src/main/java/ch/plebsapps/operatoren/KonkatenationOp.java
@@ -4,9 +4,16 @@ public class KonkatenationOp {
 
     public static void main(String[] args) {
 
+        //Unproblematisch
         System.out.println(1 + "2" + 3);   // Ausgabe  123
+
+        //PROBLEM bei Ausgabe es wird erst eine Addition ausgeführt dan erst wird der String Zusammengefügt
         System.out.println(1 + 2 + "3");   // Ausgabe  33
+
+        //Falls das 2 und 3 Element als String aus gegeben werde, soll unproblematisch
         System.out.println("1" + 2 + 3);   // Ausgabe  123
+
+        //Falls das 2 und 3 Element addiert werden soll müssen das 2 und 3 Element geklammert werden
         System.out.println("1" + (2 + 3)); // Ausgabe  15
     }
 }
