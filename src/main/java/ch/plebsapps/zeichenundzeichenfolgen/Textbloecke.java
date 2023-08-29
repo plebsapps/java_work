938e456c615d8bc3549d94d306586922547abdab
plebsapps <balz@balz-software.de>
2023-08-29 14:42:29 +0200

    Text Blöcke

---
+++ src/main/java/ch/plebsapps/zeichenundzeichenfolgen/Textbloecke.java
@@ -0,0 +1,19 @@
+package ch.plebsapps.zeichenundzeichenfolgen;
+
+public class Textbloecke {
+    public static void main(String[] args) {
+        String str = """
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Titel der Webseite</title>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <!-- weitere Kopfinformationen -->
+            <!-- Kommentare werden im Browser nicht angezeigt. -->
+          </head>
+          <body>
+            <p>Inhalt der Webseite</p>
+          </body>
+          """;
+    }
+}
