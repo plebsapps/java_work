d47edaa9e7c77e8be2b445d069f8f18b5a43d6cc
plebsapps <balz@balz-software.de>
2023-09-07 11:25:37 +0200

    Objektorientiertes Programmieren Rekords

--- src/main/java/ch/plebsapps/objectorientierung/RecordKlasse.java
+++ src/main/java/ch/plebsapps/objectorientierung/RecordKlasse.java
@@ -1,19 +1,12 @@
 package ch.plebsapps.objectorientierung;
 
-
-
 public class RecordKlasse {
     public static void main(String[] args) {
-
         City city = new City("Marburg", 12256., 1000, new Location(1,2));
         System.out.println("1" + 2 + 3);
 
-
-
     }
-
 }
-
 
 record City(
         String name,
@@ -29,5 +22,10 @@ record Location(
         int y
 ) {}
 
-
-
+record Person1(String name, int age) {
+    //Dieser explizit hingeschriebene Konstruktor wird normalerweise implizit von Compiler erstellt
+    public Person1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
